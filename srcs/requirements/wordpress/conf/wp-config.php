054b9aab645fefc35455ca7230c9af4ede85d2b1
Leonid <lporoshi@student.42heilbronn.de>
2024-10-16 01:44:24 +0200

    fix wp-config.php backslash that was left when converting a script to the actual config file

--- srcs/requirements/wordpress/conf/wp-config.php
+++ srcs/requirements/wordpress/conf/wp-config.php
@@ -6,7 +6,7 @@ define( 'DB_HOST', 'mariadb' );
 define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', '' );
 define('FS_METHOD','direct');
-\$table_prefix = 'wp_';
+$table_prefix = 'wp_';
 // define('WP_DEBUG', true);
 // define('WP_DEBUG_LOG', true);
 // define('WP_DEBUG_DISPLAY', true);
