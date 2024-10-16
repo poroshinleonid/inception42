8727ecc3e01c4bc60a2677748891855c6a91521a
Leonid <lporoshi@student.42heilbronn.de>
2024-10-16 03:32:11 +0200

    works but shows admin panel on the website

--- Makefile
+++ Makefile
@@ -2,6 +2,10 @@ NAME = inception
 
 all: up
 
+vol:
+	-@docker volume rm db-volume
+	-@docker volume rm wp-volume
+
 up: build
 	@printf "Starting ${NAME}...\n"
 	@docker-compose -f ./srcs/docker-compose.yml --env-file srcs/.env up -d
@@ -11,7 +15,7 @@ build:
 	@mkdir -p /home/${USER}/data/
 	@mkdir -p /home/${USER}/data/wordpress/
 	@mkdir -p /home/${USER}/data/mariadb/
-	@docker-compose -f ./srcs/docker-compose.yml --env-file srcs/.env up -d --build
+	@docker-compose --progress=plain -f ./srcs/docker-compose.yml --env-file srcs/.env build  --no-cache
 
 down:
 	@printf "Stopping ${NAME}...\n"
@@ -25,18 +29,20 @@ clean: down
 
 fclean:
 	@printf "Fcleaning ${NAME}...\n"
-	@docker stop $$(docker ps -qa) || true
-	@docker system prune --all --force --volumes
+	-@docker stop $$(docker ps -qa) || true
+	-@docker system prune --all --force --volumes
 	@docker network prune --force
 	@docker volume prune --force
-	@rm -rf /home/${USER}/data/*
-	@rmdir /home/${USER}/data/
+	-@docker volume rm db-volume
+	-@docker volume rm wp-volume
+	-@rm -rf /home/${USER}/data/*
+	-@rmdir /home/${USER}/data/
 
 dir:
 	@mkdir -p /home/${USER}/data
 
 .PHONY	: all build down re clean fclean
 
-#.env variables: DB_NAME DB_USER DB_PASS DB_ROOT
+#.env variables: DB_NAME DB_USER DB_PASS DB_ROOT_PASS
 #RUN sed -i "s|bind-address = 127.0.0.1|bind-address = 0.0.0.0|g" \
 	/etc/mysql/mariadb.conf.d/50-server.cnf
--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,78 +1,40 @@
-#!bin/sh
+#!/bin/sh
 
-
-
+# Check if MySQL database directory exists
 if [ ! -d "/var/lib/mysql/mysql" ]; then
+    echo "CREATING THE FOLDER FOR MYSQL" 1>&2
+    mkdir -p /var/lib/mysql/
+    mkdir -p /var/lib/mysql/mysql/
+    chown -R mysql:mysql /var/lib/mysql
 
-        chown -R mysql:mysql /var/lib/mysql
+    # Initialize the database
+    mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
 
-        # init database
-        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-
-        tfile=`mktemp`
-        if [ ! -f "$tfile" ]; then
-                return 1
-        fi
+    # Create a temporary file for SQL commands
+    tfile=`mktemp`
+    if [ ! -f "$tfile" ]; then
+        exit 1  # Use 'exit' instead of 'return' since this is a script
+    fi
 fi
 
+# Check if the WordPress database directory exists
 if [ ! -d "/var/lib/mysql/wordpress" ]; then
 
-        cat << EOF > /tmp/create_db.sql
+    # Create the SQL file to set up the database and user
+    cat << EOF > /tmp/create_db.sql
 USE mysql;
 FLUSH PRIVILEGES;
-DELETE FROM     mysql.user WHERE User='';
-DROP DATABASE test;
+DELETE FROM mysql.user WHERE User='';
+DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
+ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';
+CREATE DATABASE ${DB_NAME};
+CREATE USER '${DB_USER}'@'%' IDENTIFIED BY '${DB_PASS}';
+GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
-        # run init.sql
-        /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-        rm -f /tmp/create_db.sql
+
+    /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
+    rm -f /tmp/create_db.sql
 fi
-
-# mysql -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;"
-# mysql -e "CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASS}';"
-# mysql -e mysql -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASS}';"
-# mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PASSWORD}';"
-# mysql -e "FLUSH PRIVILEGES;"
-# mysqladmin -u root -p$SQL_ROOT_PASSWORD shutdown
-
-
-
-# if [ ! -d "/var/lib/mysql/mysql" ]; then
-
-#         chown -R mysql:mysql /var/lib/mysql
-
-#         # init database
-#         mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
-
-#         tfile=`mktemp`
-#         if [ ! -f "$tfile" ]; then
-#                 return 1
-#         fi
-# fi
-
-# if [ ! -d "/var/lib/mysql/wordpress" ]; then
-
-#         cat << EOF > /tmp/create_db.sql
-# USE mysql;
-# FLUSH PRIVILEGES;
-# DELETE FROM     mysql.user WHERE User='';
-# DROP DATABASE test;
-# DELETE FROM mysql.db WHERE Db='test';
-# DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-# ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-# CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-# CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-# GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
-# FLUSH PRIVILEGES;
-# EOF
-#         # run init.sql
-#         /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-#         rm -f /tmp/create_db.sql
-# fi
